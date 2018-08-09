352befb94e717d2d5c210e21b459446e08268640
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-09 19:19:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,12 @@ function ternaryCheckCity(city){
   return city==='NYC'? 'Ok, sounds good.':'No go.';
 }
 
-function switchOnCharmFromTip(tip){
-  switch tip{
+function switchOnCharmFromTip(tip)
+{
+  switch (tip){
     case 'generous':
     return 'Thank you so much.'
   }
 }
+
 
