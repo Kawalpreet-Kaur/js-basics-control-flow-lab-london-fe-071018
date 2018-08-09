d1de4d0a039a9388a2146375192cabaea4a96656
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-09 19:07:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(distance){
-  if (distance<=400){
+function scuberGreetingForFeet(ride){
+  if (ride<=400){
     console.log('This one is on me!');
   }
+  else if (ride)
 }
