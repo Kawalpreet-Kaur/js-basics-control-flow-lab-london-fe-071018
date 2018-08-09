47215111710d12eee253752a38108bfa8926d9c9
Kawalpreet Kaur <kawalpreet.kaur@hotmail.com>
2018-08-09 19:08:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(ride){
-  if (ride<=400){
+function scuberGreetingForFeet(ride)
+{
+  if (ride<=400)
+  {
     console.log('This one is on me!');
   }
   else if (ride)
+  {
+    
+  }
 }
